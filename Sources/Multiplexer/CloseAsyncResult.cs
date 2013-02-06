e1b2b6db6c89bdbc2dbb7cb68fa3cda40010fd1c
Andrew Au <cshung@gmail.com>
2013-02-06 13:15:11 -0800

    Coordinated Connection Shutdown - Second Attempt

---
+++ Sources/Multiplexer/CloseAsyncResult.cs
@@ -0,0 +1,21 @@
+﻿namespace Multiplexer
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Net.Sockets;
+    using System.Threading;
+
+    internal class CloseAsyncResult : AsyncResult
+    {
+        internal CloseAsyncResult(AsyncCallback callback, object state, Connection owner)
+            : base(callback, state, owner, "Close")
+        {
+        }
+
+        internal void Complete()
+        {
+            base.Complete(null);
+        }
+    }
+}
+
